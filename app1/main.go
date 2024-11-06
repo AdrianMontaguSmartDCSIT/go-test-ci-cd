f3f9ed894b38b76ffaaa2ade6c96aabaa8752c2d
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-11-06 09:29:36 +0000

    test format

--- app1/main.go
+++ app1/main.go
@@ -6,8 +6,14 @@ import (
 
 func main() {
 	fmt.Println(addNumbers(1, 2))
+	fmt.Println(plusOne(1))
 }
 
 func addNumbers(a, b int) int {
 	return a + b
+}
+
+func plusOne(a int) int {
+	a = a + 1
+	return a
 }
